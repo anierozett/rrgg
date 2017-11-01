9c664bad5dbfe5401f24bc2fe35fd57553e83e7a
anierozett <anierozett@gmail.com>
2017-11-01 12:28:13 +0900

    たたき台
    学習者は一様分布で0.5以上の時PG1を評価します
    ガンマは0.001にしています

---
+++ test_1.py
@@ -0,0 +1,24 @@
+﻿from numpy.random import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+pg1 = [5.000e-1,5.000e-1,5.000e-1,5.000e-1,5.000e-1,5.000e-1,5.000e-1,5.000e-1,5.000e-1,5.000e-1]
+pg2 = [5.000e-1,5.000e-1,5.000e-1,5.000e-1,5.000e-1,5.000e-1,5.000e-1,5.000e-1,5.000e-1,5.000e-1]
+
+for i in range(100000):
+    for j in range(10):
+        a = rand() #ベルヌーイ分布にする
+        #a = 1
+        if a >= 0.1:
+            pg1[j] = 0.001 + 0.999 * pg1[j]
+            pg2[j] = 0.999 * pg2[j]
+        else:
+            pg1[j] = 0.999 * pg1[j]
+            pg2[j] = 0.001 + 0.999 * pg2[j]
+        #print a
+print pg1
+
+left = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+plt.bar(left, pg1)
+
+plt.show()
